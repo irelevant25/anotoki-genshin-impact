87c28e0131d2ade7268d56a634abed4b36631182
Frantisek Pastorek <pastorek@alfabase.sk>
2025-05-29 03:19:13 +0200

    Add new materials assets. Fix weapons individual scripts names.

---
+++ data/database/weapons/APPRENTICES_NOTES.js
@@ -0,0 +1,119 @@
+var APPRENTICES_NOTES = {
+    "name": "Apprentice's Notes",
+    "icon": "assets/weapons/Apprentice's Notes.avif",
+    "icon_ascension": "assets/weapons/Apprentice's Notes - ascension.avif",
+    "type": "Catalyst",
+    "quality": "1",
+    "how_to_obtain": "ChestsInvestigationSold by SchulzComes bundled with new catalyst characters",
+    "release_date": "September 28, 2020",
+    "secondary_stat": "None",
+    "effects": [],
+    "refinements": [],
+    "ascensions_materials_and_stats": [
+        {
+            "phase": 0,
+            "levels": [],
+            "cost": [
+                {
+                    "name": "Mora",
+                    "value": 0,
+                    "icon": "assets/materials/Mora.avif"
+                },
+                {
+                    "name": "Tile of Decarabian's Tower",
+                    "value": 1,
+                    "icon": "assets/materials/Tile of Decarabian's Tower.avif"
+                },
+                {
+                    "name": "Heavy Horn",
+                    "value": 1,
+                    "icon": "assets/materials/Heavy Horn.avif"
+                },
+                {
+                    "name": "Damaged Mask",
+                    "value": 1,
+                    "icon": "assets/materials/Damaged Mask.avif"
+                }
+            ]
+        },
+        {
+            "phase": 1,
+            "levels": [],
+            "cost": [
+                {
+                    "name": "Mora",
+                    "value": 5000,
+                    "icon": "assets/materials/Mora.avif"
+                },
+                {
+                    "name": "Debris of Decarabian's City",
+                    "value": 1,
+                    "icon": "assets/materials/Debris of Decarabian's City.avif"
+                },
+                {
+                    "name": "Heavy Horn",
+                    "value": 4,
+                    "icon": "assets/materials/Heavy Horn.avif"
+                },
+                {
+                    "name": "Damaged Mask",
+                    "value": 2,
+                    "icon": "assets/materials/Damaged Mask.avif"
+                }
+            ]
+        },
+        {
+            "phase": 2,
+            "levels": [],
+            "cost": [
+                {
+                    "name": "Mora",
+                    "value": 5000,
+                    "icon": "assets/materials/Mora.avif"
+                },
+                {
+                    "name": "Debris of Decarabian's City",
+                    "value": 2,
+                    "icon": "assets/materials/Debris of Decarabian's City.avif"
+                },
+                {
+                    "name": "Black Bronze Horn",
+                    "value": 2,
+                    "icon": "assets/materials/Black Bronze Horn.avif"
+                },
+                {
+                    "name": "Stained Mask",
+                    "value": 2,
+                    "icon": "assets/materials/Stained Mask.avif"
+                }
+            ]
+        },
+        {
+            "phase": 3,
+            "levels": [],
+            "cost": [
+                {
+                    "name": "Mora",
+                    "value": 10000,
+                    "icon": "assets/materials/Mora.avif"
+                },
+                {
+                    "name": "Fragment of Decarabian's Epic",
+                    "value": 1,
+                    "icon": "assets/materials/Fragment of Decarabian's Epic.avif"
+                },
+                {
+                    "name": "Black Bronze Horn",
+                    "value": 4,
+                    "icon": "assets/materials/Black Bronze Horn.avif"
+                },
+                {
+                    "name": "Stained Mask",
+                    "value": 3,
+                    "icon": "assets/materials/Stained Mask.avif"
+                }
+            ]
+        }
+    ],
+    "version": "1.0"
+}
